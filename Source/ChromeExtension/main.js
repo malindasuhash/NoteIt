3d69769cf5fb2012ec78fdbda60b4fe6b750fe7e
malindasuhash <malinda.suhash@gmail.com>
2014-11-14 06:34:01 +0000

    Adding the manifest file with a background event.

--- Source/ChromeExtension/main.js
+++ Source/ChromeExtension/main.js
@@ -1,1 +1,14 @@
-// Placeholder for JavaScript.
+// Context menu for selection
+
+var appInfo = {
+    name: 'NoteIt!',
+    contextId: 'noteItId'
+};
+
+(function () {
+    chrome.contextMenus.create({
+        "id": appInfo.contextId,
+        "title": appInfo.name,
+        "contexts": ['selection']
+    });
+})();
